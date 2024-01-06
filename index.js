8a7db6b96c71857a771d2be672fead1e872ff0c7
Osama daoud <osamadaoud66@gmail.com>
2024-01-06 05:57:55 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b){
+return a+b;
+}
+
+function subtract(a,b){
+return a-b
+}
+
+function multiply(a,b){
+return a*b
+}
+
+function divide(a,b){
+return a/b
+}
+add(5,6);
+subtract(5,6);
+multiply(5,6);
+divide(5,6);
+
+function makeInt(string){
+    return parseInt(string,10)
+}
+function preserveDecimal(string){
+    return parseFloat(string,2)
+}
+
+
+
+
